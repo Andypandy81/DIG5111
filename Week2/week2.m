ae0b6eea16df116a8572f20bf49c33d5c68ee79d
Andre Smith <andresmith@Andres-MBP.broadband>
2025-02-04 22:43:25 +0000

    task 2 update

--- Week2/week2.m
+++ Week2/week2.m
@@ -1,4 +1,4 @@
-
+%Task 1
 %audio filename must in same directory of this m file
 Filename = 'piano_middle_C.wav';
 %Fist read wav file into memory 
@@ -12,4 +12,33 @@ plot(Time,sig);
 ylabel('Amplitude');
 xlabel('Time Sec');
 
-audioinfo ("piano_middle_C.wav")
+%task 2
+%1)
+%to get the file size you use all the file data? then time the durarion
+%by the samplefrequency and the bit depth then divide by 8 ti get to the
+%size of a bit? (ask sam for clarificaiton)
+info1 = Duration*44100*16/8
+disp(Filename)
+
+%2)
+info1 = audioinfo("piano_middle_C.wav");
+audioinfo(Filename);
+
+%3) Create code to input the full signal calle
+
+t1 = 0.5;
+t2 = 1;
+%sample indicies
+t1samples = t1*Fs;
+t2samples = t2*Fs;
+partSig = sig(t1samples:t2samples);
+partTime = Time(t1samples:t2samples)
+plot(partTime ,partSig)
+audioread("piano_middle_C.wav")
+
+%4)
+sig_croped = sig(Fs*0.5:sig*1);
+plot(sig_croped)
+
+
+
